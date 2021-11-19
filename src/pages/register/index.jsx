19e10a7b682d542bffc439f787b8444e382e40f7
JoaoVasconcelosV <65971039+JoaoVasconcelosV@users.noreply.github.com>
2021-11-19 20:12:18 -0300

    Criação da tela register

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+
+export default function Register() {
+  return (
+    <div className="register"> 
+      <Header />
+      <div className="content">
+        <h1>Registrar</h1>
+      </div>
+    </div>
+  )
+};
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 export default function Routes () {
   return (
     <Router>
       <Route path="/" exact component={Home} />
+      <Route path="/register" component={Register} />
     </Router>  
   )
 }
